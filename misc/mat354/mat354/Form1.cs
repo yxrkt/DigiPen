592fc208703ff34f0e041dbb38e9268ac991bb78
Alex Serio <h4tf4k@gmail.com>
2008-04-16 22:45:37 +0000

    fix sort

--- misc/mat354/mat354/Form1.cs
+++ misc/mat354/mat354/Form1.cs
@@ -13,12 +13,17 @@ namespace mat354
 {
 public partial class Form1 : Form
 {
+  // Consts
+  const int BIG_NUMBER = 0x4FF;
+
   // Data
   byte[] rgbs;
   List<Point> points;
+  Point[] region;
   Bitmap diagram;
   int k = 1;
   byte[] color;
+  List<Region> regions;
 
   // Functions
   public Form1()
@@ -53,8 +58,17 @@ public partial class Form1 : Form
     int step   = stride / width;
     int nBytes = stride * height;
     rgbs       = new byte[nBytes];
-    for ( int i = 0; i < nBytes; ++i )
-      rgbs[i] = 255;
+
+    if ( points.Count > k )
+    {
+      FillRegions( width, height );
+    }
+    else
+    {
+      for ( int i = 0; i < nBytes; ++i )
+        rgbs[i] = 255;
+    }
+
     SetPointHandles();
 
     Marshal.Copy( rgbs, 0, data.Scan0, nBytes );
@@ -65,6 +79,70 @@ public partial class Form1 : Form
 
     // Update displayed image
     pictureBox.Image = diagram;
+  }
+
+  private void FillRegions( int width, int height )
+  {
+    Point tester = new Point();
+
+    // For each pixel method
+    for ( int y = 0; y < height; ++y )
+    {
+      for ( int x = 0; x < width; ++x )
+      {
+        Point[] nears = new Point[k];
+        tester.X = x;
+        tester.Y = y;
+        Region region = GetRegion( tester, false );
+
+        int found = -1;
+        int index = 0;
+        foreach ( Region r in regions )
+        {
+          if ( r.indices.Equals( (object)region ) )
+          {
+            found = index;
+            break;
+          }
+          index++;
+        }
+
+        if ( found != -1 )
+          region = regions[found];
+        else
+          regions.Add( region );
+
+        for ( int i = 0; i < 3; ++i )
+          rgbs[3 * ( y * width + x ) + i] = region.color[i];
+      }
+    }
+  }
+
+  private Region GetRegion( Point cur, bool exclude )
+  {
+    int nPoints = points.Count;
+
+    //if ( !exclude )
+    //{
+      List<RelPoint> rpoints = new List<RelPoint>( nPoints );
+
+      int index = 0;
+      foreach ( Point p in points )
+      {
+        int x = p.X, y = p.Y;
+        rpoints.Add( new RelPoint( x * x + y * y, index++ ) );
+      }
+
+      rpoints.Sort();
+
+      rpoints.RemoveRange( k, nPoints - 1 );
+
+      return new Region( rpoints );
+    //}
+    //else
+    //{
+    //  List<RelPoint> rpoints = new List<RelPoint>( nPoints - 1 );
+    //}
   }
 
   private void SetPointHandles()
@@ -99,58 +177,115 @@ public partial class Form1 : Form
         rgbs[step * ( ( y - 1 ) * width + ( x - 2 ) ) + i] = color[i];
     }
   }
+
+
 }
 
 class Rainbow
 {
   static int count = 0;
 
-  static public Color GetNext()
+  static public byte[] GetNext()
   {
-    Color c = new Color();
+    byte[] color = new byte[3];
 
-    switch ( count )
-    {
-      //case 0: // RED
-      //  c.R = 255;
-      //  c.G = 0;
-      //  c.B = 0;
-      //  break;
-      //case 1: // ORANGE
-      //  c.R = 255;
-      //  c.G = 128;
-      //  c.B = 0;
-      //  break;
-      //case 2: // YELLOW
-      //  c.R = 255;
-      //  c.G = 0;
-      //  c.B = 255;
-      //  break;
-      //case 3: // GREEN
-      //  c.R = 0;
-      //  c.G = 255;
-      //  c.B = 0;
-      //  break;
-      //case 4: // BLUE
-      //  c.R = 0;
-      //  c.G = 0;
-      //  c.B = 255;
-      //  break;
-      //case 5: // INDIGO
-      //  c.R = 128;
-      //  c.G = 0;
-      //  c.B = 255;
-      //  break;
-      //case 6: // VIOLET
-      //  c.R = 255;
-      //  c.G = 0;
-      //  c.B = 255;
-      //  break;
-    }
+    //switch ( count )
+    //{
+    //  case 0: // RED
+    //    c.R = 255;
+    //    c.G = 0;
+    //    c.B = 0;
+    //    break;
+    //  case 1: // ORANGE
+    //    c.R = 255;
+    //    c.G = 128;
+    //    c.B = 0;
+    //    break;
+    //  case 2: // YELLOW
+    //    c.R = 255;
+    //    c.G = 0;
+    //    c.B = 255;
+    //    break;
+    //  case 3: // GREEN
+    //    c.R = 0;
+    //    c.G = 255;
+    //    c.B = 0;
+    //    break;
+    //  case 4: // BLUE
+    //    c.R = 0;
+    //    c.G = 0;
+    //    c.B = 255;
+    //    break;
+    //  case 5: // INDIGO
+    //    c.R = 128;
+    //    c.G = 0;
+    //    c.B = 255;
+    //    break;
+    //  case 6: // VIOLET
+    //    c.R = 255;
+    //    c.G = 0;
+    //    c.B = 255;
+    //    break;
+    //}
 
     count = count % 7;
-    return c;
+    return color;
   }
+}
+
+class RelPoint
+{
+  public RelPoint( int cost_, int index_ )
+  {
+    cost  = cost_;
+    index = index_;
+  }
+
+  static public bool operator <( RelPoint lhs, RelPoint rhs )
+  {
+    return ( lhs.cost < rhs.cost );
+  }
+
+  static public bool operator >( RelPoint lhs, RelPoint rhs )
+  {
+    return ( lhs.cost > rhs.cost );
+  }
+
+  public int cost;
+  public int index;
+}
+
+class Region
+{
+  public Region( List<RelPoint> rpoints )
+  {
+    indices = new List<int>( rpoints.Count );
+    foreach ( RelPoint p in rpoints )
+      indices.Add( p.index );
+    indices.Sort();
+
+    color = Rainbow.GetNext();
+
+    //index = count++;
+  }
+
+  public Region( int[] indices_ )
+  {
+    indices = new List<int>( indices_.Length );
+    foreach ( int i in indices_ )
+      indices.Add( i );
+    indices.Sort();
+
+    color = Rainbow.GetNext();
+
+    //index = count++;
+  }
+
+  public List<int> indices;
+  public byte[] color;
+  //public int index;
+
+  //static public int count = 0;
 }
 
 }
